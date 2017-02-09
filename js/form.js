5d6bc91fb8ec953418864497ce15c773302b20a5
Roman Milkovic <destabiliseus@gmail.com>
2017-02-09 20:32:09 +0800

    Исправил ошибки

--- js/form.js
+++ js/form.js
@@ -1,7 +1,6 @@
 'use strict';
 
 var pins = document.querySelectorAll('.pin');
-var pinActive = document.querySelector('.pin--active');
 var dialog = document.querySelector('.dialog');
 var dialogClose = dialog.querySelector('.dialog__close');
 
@@ -16,48 +15,65 @@ var noticeCapacity = noticeForm.querySelector('#capacity');
 var noticeRoomNumber = noticeForm.querySelector('#room_number');
 
 // Добавляем активный класс к пину
-function addActivePin(pin) {
+var highlightPin = function (pin) {
   pin.classList.add('pin--active');
-}
+};
 
 // Проверяем наличие активного pin и убираем его если есть
-function removeActivePin() {
-  if (pinActive) {
-    pinActive.classList.remove('pin--active');
-  }
-}
-
-// После клика на pin, убираем активный класс у всех pins, добавляем активным класс к текущему pin и показываем диалоговое окно
-function clickOnPin(event) {
-  removeActivePin();
-  addActivePin(event.currentTarget);
-  dialog.style.display = 'block';
-}
-
-// Делаем pin неактивным и закрываем диологовое окно
-dialogClose.addEventListener('click', function (event) {
-  removeActivePin();
-  dialog.style.display = 'none';
-});
-
-// Вещаем на все pins обработчик событий на клик
-var deactivateAllPins = function () {
+var deactivatePins = function () {
   for (var i = 0; i < pins.length; i++) {
-    pins[i].addEventListener('click', clickOnPin);
+    var pin = pins[i];
+    pin.classList.remove('pin--active');
   }
 };
 
-deactivateAllPins();
+// Метод для показа диалога
+var showDialog = function () {
+  deactivatePins();
+  highlightPin(event.currentTarget);
+  dialog.style.display = 'block';
+};
+
+// Метод для скрытия диалога
+var hideDialog = function () {
+  deactivatePins();
+  dialog.style.display = 'none';
+};
+
+// После клика на pin, убираем активный класс у всех pins, добавляем активным класс к текущему pin и показываем диалоговое окно
+var pinClickHandler = function (event) {
+  showDialog();
+};
+
+var dialogCloseEventHandler = function () {
+  hideDialog();
+};
+
+// Делаем pin неактивным и закрываем диологовое окно
+dialogClose.addEventListener('click', dialogCloseEventHandler);
+
+// Вещаем на все pins обработчик событий на клик
+var initHandlers = function () {
+  for (var i = 0; i < pins.length; i++) {
+    pins[i].addEventListener('click', pinClickHandler);
+  }
+};
+
+initHandlers();
 
 // Задаем инпуты определенные требования валидации
-noticeTitle.required = true;
-noticeTitle.minLength = 30;
-noticeTitle.maxLength = 100;
-noticePrice.required = true;
-noticePrice.type = 'number';
-noticePrice.min = 1000;
-noticePrice.max = 1000000;
-noticeAddress.required = true;
+var setupFormValidation = function () {
+  noticeTitle.required = true;
+  noticeTitle.minLength = 30;
+  noticeTitle.maxLength = 100;
+  noticePrice.required = true;
+  noticePrice.type = 'number';
+  noticePrice.min = 1000;
+  noticePrice.max = 1000000;
+  noticeAddress.required = true;
+};
+
+setupFormValidation();
 
 // Синхронизируем время въезда/выезда
 var timeSync = function () {
@@ -76,34 +92,24 @@ var timeOutSync = function () {
   }
 };
 
-// Добавляем обработчик событий на изменения
-noticeTime.addEventListener('change', timeSync);
-noticeTimeOut.addEventListener('change', timeOutSync);
-
 // В зависимости от выбранного типа устанавливаем необходимую цену
 var typeSync = function () {
-  var noticeTypeSelectedOption = noticeType.selectedIndex;
-  // console.log(noticeTypeSelectedOption);
+  var noticeTypeSelectedOption = noticeType.options[noticeType.selectedIndex].value;
   switch (noticeTypeSelectedOption) {
-    case 0:
+    case 'flat':
       noticePrice.min = 1000;
       break;
-    case 1:
+    case 'shack':
       noticePrice.min = 0;
       break;
-    case 2:
+    case 'castle':
       noticePrice.min = 10000;
       break;
   }
 };
 
-// Добавляем обработчик событий на изменения
-noticeType.addEventListener('change', typeSync);
-
-
-// Синхронизиурем кол-во комнат и гостей
 var roomNumberSync = function () {
-  if (noticeRoomNumber.options[0].selected === true) {
+  if (noticeRoomNumber.options[0].selected) {
     noticeCapacity.options[1].selected = true;
   } else {
     noticeCapacity.options[0].selected = true;
@@ -111,11 +117,14 @@ var roomNumberSync = function () {
 };
 
 var capacitySync = function () {
-  if (noticeCapacity.options[1].selected === true) {
+  if (noticeCapacity.options[1].selected) {
     noticeRoomNumber.options[0].selected = true;
   }
 };
 
 // Добавляем обработчик событий на изменения
+noticeType.addEventListener('change', typeSync);
+noticeTime.addEventListener('change', timeSync);
+noticeTimeOut.addEventListener('change', timeOutSync);
 noticeRoomNumber.addEventListener('change', roomNumberSync);
 noticeCapacity.addEventListener('change', capacitySync);
