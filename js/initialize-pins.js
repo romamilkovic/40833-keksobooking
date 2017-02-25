056d14bd51ffcf714a7f88629510a688b0e39825
Roman Milkovic <destabiliseus@gmail.com>
2017-02-25 22:40:50 +0800

    Черновик 2

--- js/initialize-pins.js
+++ js/initialize-pins.js
@@ -1,13 +1,12 @@
-
 'use strict';
 
 window.initializePins = (function () {
   return function () {
+
     var pinsMap = document.querySelector('.tokyo__pin-map');
-    var pins = document.querySelectorAll('.pin');
-    var highlightedPin = document.querySelector('.pin--active');
-    var dataUrl = 'https://intensive-javascript-server-pedmyactpq.now.sh/keksobooking/data';
-    var similarApartments = [];
+    var pinClicked;
+    var URL = 'https://intensive-javascript-server-pedmyactpq.now.sh/keksobooking/data';
+    // var similarApartments = [];
 
     // Добавляем активный класс к пину
     var highlightPin = function (pin) {
@@ -17,6 +16,7 @@ window.initializePins = (function () {
 
     // Проверяем наличие активного pin и убираем его если есть
     var deactivatePins = function () {
+      var pins = document.querySelectorAll('.pin');
       for (var i = 0; i < pins.length; i++) {
         var pin = pins[i];
         pin.classList.remove('pin--active');
@@ -24,73 +24,133 @@ window.initializePins = (function () {
       }
     };
 
+    var openDialogHandler = function (event) {
+      deactivatePins();
+
+      pinClicked = event.target.closest('.pin');
+
+      if (pinClicked) {
+        highlightPin(pinClicked);
+      }
+    };
+
+    // Закрываем окно, очищаем активные pins и возвращаем focus
+    var closeDialogHandler = function (pin, openFromKeyboard) {
+      deactivatePins();
+      pinClicked.focus();
+    };
+
+    var createPin = (function () {
+      var pinTemplate = document.querySelector('#pin-template');
+      var pinToClone = pinTemplate.content.querySelector('.pin');
+      var pinWidth = 56;
+      var pinHeight = 75;
+
+      return function (element, index) {
+        var pinNew = pinToClone.cloneNode(true);
+        var pinImg = pinNew.querySelector('img');
+
+        // Для точного местоположение используем высоту/ширину pin
+        pinNew.style.top = (element.location.y - pinHeight) + 'px';
+        pinNew.style.left = (pinWidth / 2 + element.location.x) + 'px';
+
+        // Внутри pin ранее нашли картинку, теперь задаем ей src и addEventListener
+        pinImg.src = element.author.avatar;
+        pinImg.alt = 'User Avatar';
+
+        pinNew.addEventListener('click', function (event) {
+          openDialogHandler(event);
+          window.showCard(closeDialogHandler, element);
+        });
+
+        pinNew.addEventListener('keydown', function (event) {
+          if (window.utils.isKeyEnter(event)) {
+            openDialogHandler(event);
+            window.showCard(closeDialogHandler, element);
+          }
+        });
+
+        return pinNew;
+      };
+    })();
 
     // Восстанавливаем фокус
-    function restorePinFocus(pin) {
-      pin.focus();
-    }
+    // var restorePinFocus = function (pin) {
+    //   pin.focus();
+    // };
 
     // Убираем активный класс у pin, если диал. окно было открыто с клавиатуры, то при закрытии
     // возвращаем фокус на pin
-    function closeDialogHandler(pin, openFromKeyboard) {
-      return function () {
-        deactivatePins();
-        if (openFromKeyboard) {
-          restorePinFocus(pin);
-        }
-      };
-    }
+    // var closeDialogHandler = function (pin, openFromKeyboard) {
+    //   return function () {
+    //     deactivatePins();
+    //     if (openFromKeyboard) {
+    //       restorePinFocus(pin);
+    //     }
+    //   };
+    // };
+
+    // Рендерим pins
+    var renderPins = function (data) {
+      data = data.slice(0, 3); // берем только первые три квартиры
+
+      data.forEach(function (element, i) {
+        var renderedPin = createPin(element, i);
+        pinsMap.appendChild(renderedPin); // вставка пина в указанное место в DOM
+      });
+    };
 
     // Работаем с pins с помощью делегирования
     // цикл двигается вверх от target до .tokyo__pin-map
     // затем находим нужный нам элемент с помощью метода contains.
-    var pinsMapHandler = function (event, openFromKeyboard) {
-      var target = event.target;
-      while (target !== pinsMap) {
-        if (target.classList.contains('pin')) {
-          deactivatePins();
-          window.showCard(closeDialogHandler(target, openFromKeyboard));
-          highlightPin(target);
-          return;
-        }
-        target = target.parentNode;
-      }
-    };
+    // var pinsMapHandler = function (event, openFromKeyboard) {
+    //   var target = event.target;
+    //   while (target !== pinsMap) {
+    //     if (target.classList.contains('pin')) {
+    //       deactivatePins();
+    //       window.showCard(closeDialogHandler(target, openFromKeyboard));
+    //       highlightPin(target);
+    //       return;
+    //     }
+    //     target = target.parentNode;
+    //   }
+    // };
 
     // Ловим событие на enter
-    var pinsMapKeyDownHandler = function (event) {
-      if (window.utils.isKeyEnter(event)) {
-        pinsMapHandler(event, true);
-      }
-    };
+    // var pinsMapKeyDownHandler = function (event) {
+    //   if (window.utils.isKeyEnter(event)) {
+    //     pinsMapHandler(event, true);
+    //   }
+    // };
 
     // Если pin изначально активный, то вешаем на него обработчик
-    if (highlightedPin) {
-      window.showCard(closeDialogHandler(highlightedPin));
-    }
+    // if (highlightedPin) {
+    //   window.showCard(closeDialogHandler(highlightedPin));
+    // }
 
     // Создаем функцию callback, создаем новый объект с данными из первых трех элементов,
     // создаем новый fragment и добавляем в DOM каждый из этих элементов
-    var onLoad = function (data) {
-      similarApartments = data;
-      var fragment = document.createDocumentFragment();
-      var dialogFragment = document.createDocumentFragment();
-      var slicedSimilarApartments = similarApartments.slice(0, 3);
+    // var onLoad = function (data) {
+    //   similarApartments = data;
+    //   var fragment = document.createDocumentFragment();
+    //   var slicedSimilarApartments = similarApartments.slice(0, 3);
+    //
+    //   slicedSimilarApartments.forEach(function (element, index) {
+    //     fragment.appendChild(window.createPin(element, index));
+    //   });
+    //
+    //   pinsMap.appendChild(fragment);
+    // };
+    //
+    // window.load(URL, onLoad);
 
-      slicedSimilarApartments.forEach(function (element, index) {
-        fragment.appendChild(window.createPin(element, index));
-        dialogFragment.appendChild(window.createDialog(element, index));
-      });
-
-      pinsMap.appendChild(fragment);
-      pinsMap.appendChild(dialogFragment);
-
-    };
-
-    window.load(dataUrl, onLoad);
+    // Грузим данные, отрисовываем pins
+    window.load(URL, function (data) {
+      renderPins(data);
+    });
 
     // Добавляем обработчики для действий с pins
-    pinsMap.addEventListener('click', pinsMapHandler);
-    pinsMap.addEventListener('keydown', pinsMapKeyDownHandler);
+    // pinsMap.addEventListener('click', pinsMapHandler);
+    // pinsMap.addEventListener('keydown', pinsMapKeyDownHandler);
   };
 })();
--- js/show-card.js
+++ js/show-card.js
@@ -1,35 +1,89 @@
 'use strict';
 
 window.showCard = (function () {
-  return function (onCloseCallback) {
-    var dialog = document.querySelector('.dialog');
-    var dialogClose = dialog.querySelector('.dialog__close');
+  var dialog = document.querySelector('.dialog');
+  var featuresContainer = dialog.querySelector('.lodge__features');
+  var dialogClose = document.querySelector('.dialog__close');
+  var dialogCloseHandler;
 
-    var closeClickDialogHandler = function () {
-      hideDialog();
-    };
-
-    dialogClose.addEventListener('click', closeClickDialogHandler);
-
-    var hideDialog = function () {
-      dialog.style.display = 'none';
-      dialog.setAttribute('aria-hidden', true);
-      document.removeEventListener('keydown', closeKeydownHandler);
-      dialogClose.removeEventListener('click', closeClickDialogHandler);
-
-      if (window.utils.isFunction(onCloseCallback)) {
-        onCloseCallback();
-      }
-    };
-
-    function closeKeydownHandler(event) {
-      if (window.utils.isKeyEsc(event)) {
-        hideDialog();
-      }
+  var renderFeatures = function (features) {
+    featuresContainer.innerHTML = '';
+    for (var i = 0; i < features.length; i++) {
+      var span = document.createElement('span');
+      span.classList.add('feature__image');
+      span.classList.add('feature__image--' + features[i]);
+      featuresContainer.appendChild(span);
     }
+  };
 
+  var renderPhotos = function (photos) {
+    var photosContainer = dialog.querySelector('.lodge__photos');
+    photosContainer.innerHTML = '';
+    var photoWidth = 52;
+    var photoHeight = 42;
+    for (var i = 0; i < photos.length; i++) {
+      var photo = document.createElement('img');
+      photo.src = photos[i];
+      photo.alt = 'Lodge photo';
+      photo.width = photoWidth;
+      photo.height = photoHeight;
+      photosContainer.appendChild(photo);
+    }
+  };
+
+  var renderDialog = function (data) {
+    dialog.querySelector('.dialog__title img').src = data.author.avatar;
+    dialog.querySelector('.lodge__title').textContent = data.offer.title;
+    dialog.querySelector('.lodge__address').textContent = data.offer.address;
+    dialog.querySelector('.lodge__price').textContent = data.offer.price + '₽/ночь';
+    dialog.querySelector('.lodge__type').textContent = data.offer.type[data.offer.type];
+    dialog.querySelector('.lodge__rooms-and-guests').textContent = data.offer.rooms + ' комнат для ' + data.offer.rooms + ' гостей';
+    dialog.querySelector('.lodge__checkin-time').textContent = 'Заед после ' + data.offer.checkin + ' выезд до ' + data.offer.checkout;
+    dialog.querySelector('.lodge__description').textContent = data.offer.description;
+    renderPhotos(data.offer.photos);
+    renderFeatures(data.offer.features);
+  };
+
+  var closeDialog = function (event) {
+    dialog.style.display = 'none';
+    dialog.setAttribute('aria-hidden', true);
+
+    if (window.utils.isFunction(dialogCloseHandler)) {
+      dialogCloseHandler();
+    }
+  };
+
+  var dialogCloseOnEscEventListener = function (event) {
+    if (window.utils.isKeyEsc(event)) {
+      closeDialog();
+    }
+  };
+
+  var dialogCloseOnClickEventListener = function () {
+    closeDialog();
+  };
+
+  var dialogCloseOnEnterEventListener = function () {
+    if (window.utils.isKeyEnter(event)) {
+      closeDialog();
+    }
+  };
+
+  // Обработчик события закрытия окна по нажатию на ESC
+  document.addEventListener('keydown', dialogCloseOnEscEventListener);
+
+  // Обработчик события закрытия окна по клику на крестик
+  dialogClose.addEventListener('click', dialogCloseOnClickEventListener);
+
+  // Обработчик события закрытия окна по нажатию на ENTER по крестику
+  dialogClose.addEventListener('keydown', dialogCloseOnEnterEventListener);
+
+  return function (callback, data) {
     dialog.style.display = 'block';
     dialog.setAttribute('aria-hidden', false);
-    document.addEventListener('keydown', closeKeydownHandler);
+
+    dialogCloseHandler = callback;
+
+    renderDialog(data);
   };
 })();
